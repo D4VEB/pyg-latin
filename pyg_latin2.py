71ceb1425e9f230688316542f77da14f05893973
D4VEB <dhblodgett@gmail.com>
2016-03-01 07:38:58 -0800

    Added files via upload

---
+++ pyg_latin2.py
@@ -0,0 +1,15 @@
+# Jeff, this code does not work, but I want to upload it so you can see where I am wrong.
+# There is a syntax error at line 9, but I don't understand why.
+# Process finished with exit code 1
+
+your_word = input('Tell me a word')
+first_letter = your_word[0]
+
+if first_letter == "a" or first_letter == "e" or first_letter == "i" or first_letter == "o" or first_letter == "u":
+    new_word = your_word[1+] + "say"
+    print(new_word)
+    elif:
+        new_word = your_word + first_letter + "ay"
+        print(new_word)
+else:
+    print('Tell me a real word. No numbers please.')
